20875d822eb1c6135b1cda16b299e0b90b47b980
Audrey Yoo <120416826+audreyyoo@users.noreply.github.com>
2022-12-12 18:53:31 -0500

    Add files via upload

---
+++ CrackingCaesarCipher/vocabulary.py
@@ -0,0 +1,79 @@
+#Advanced Encryption Standard (AES) Cipher: one of the most common ciphers used today.
+#Algorithm: a specific set of instructions used to perform a task.
+#Bias: something in favor or against a specific group of people, usually relating to race, ethnicity, sexuality, religion, ability, gender, and more.
+#Explicit bias: decisions based on conscious beliefs about a group of people.
+#Implicit bias: decisions based on subconscious beliefs about a group of people.
+#Binary: a number system that uses only zeros and ones.
+#Bit: a unit of data in the binary number system, either a zero or one.
+#Bit-key: the number of bits (zeros and ones) in a key.
+#Bitlocker: an app used to encrypt files at rest.
+#Boolean: a data type that stores either true or false.
+#Brute-force technique: a technique to crack a cipher where the cracker tries every possible decryption key for the cipher.
+#Caesar cipher: a substitution cipher where each letter of the plaintext is replaced by a letter that is shifted down the alphabet by a fixed number.
+#CIA Triad: CIA stands for confidentiality, integrity, and availability which are the principles used to guide how we protect data and access critical services.
+#Confidentiality: ensures only authorized users have access to data.
+#Integrity: ensures that data can be trusted and has not been tampered with.
+#Cipher: a set of rules used to convert between plaintext and ciphertext.
+#Cipher machine: a machine used for encrypting and decrypting messages.
+#Ciphertext: text that appears unreadable since it was scrambled by a set of rules.
+#Concatenation: to join or link two values together.
+#Conditional: checks if a set of rules (or statement) are met and then decides which actions are performed if the rules or statement is true or false.
+#Crack a cipher: attempting to decode a secret message without knowing the specifics of the cipher and key.
+#Cryptanalyst: works to try and break secure ciphers or systems to evaluate their effectiveness.
+#Cryptographer: works to develop secure ciphers or systems.
+#Cryptography: the science of secret messages. The goal of cryptography is to make it impossible for an unauthorized party to access or read protected messages or data.
+#Data breach: an incident where information is taken without permission.
+#Data type: the classification of the item that represents what operations can be done with it. Common data types include strings, integers, and booleans.
+#Debug: the process of finding and fixing errors in code.
+#Decryption: the reverse of encryption. Decryption converts ciphertext to plaintext.
+#else: a keyword used in conditionals that occurs after the if statement. You can think of this as “otherwise”.
+#Encryption: the method used to protect or hide secret messages. Encryption scrambles up the data in a way that makes it unreadable. Encryption converts plaintext to ciphertext.
+#FileVault: an app used to encrypt files at rest.
+#find() method: returns the index of the first occurrence of a string value inside of another string.
+#Flowchart: a graphical representation of a computer program.
+#Fork: a modified version of an uploaded project.
+#Frequency Analysis: compares the ciphertext’s most frequently used letters to the most frequently used letters in English, or whatever language the message is in that you are interpreting.
+#Function: a list of steps in a program that are wrapped up together to accomplish a certain task.
+#Greater than: a comparison operator used to determine whether a number is bigger than the second number or rest of the numbers. Represented by the symbol >.
+#if: a keyword used in conditionals to specify an action that will be completed if the condition is true.
+#Initial message: the original message before performing encryption or decryption.
+#Initial position: the original index of a character in a string.
+#Input: the signals, data values, or instructions sent to a computer.
+#Integer: a data type that stores whole numbers (both positive and negative).
+#Key: what “locks” a message so only certain people can read it.
+#len() function: returns the number of characters in a string.
+#Less than: a comparison operator used to determine whether a number is smaller than the second number or rest of the numbers. Represented by the symbol <.
+#Library: stores a collection of commands and tasks that can be used in a program.
+#Loop: repeats a sequence of code over and over again.
+#for-loop: a type of loop that repeats for a specific number of times.
+#while-loop: a type of loop that continues to repeat while the condition is true.
+#lower() method: a built in method used to convert string characters to lowercase characters.
+#Method: a built in function or chunks of code that a programming language already knows without us having to give it directions. Methods are specific to an object and type.
+#Nested conditional: a conditional statement within another conditional statement.
+#Output: any information that is processed by and sent out from a computing device.
+#Penetration (Pen) Tester: trusted people who simulate cyberattacks on a company’s computer system and networks to help identify any weaknesses.
+#Plaintext: readable text that has not been changed in any way.
+#print() function: a function to display data or information into the console.
+#Pseudocode: used by programmers to plan out their program in plain language before writing any code. Used to focus on logic and not worry about syntax.
+#range() function: a built-in function that generates a sequence of integers by defining a start and the end point of the range.
+#Scope: the area in a program where a variable is recognized.
+#Shifted position: the new index value after the key was added or subtracted to the initial position.
+#Shifted message: the message after performing encryption or decryption.
+#Signal: an encryption app used for messaging and communication.
+#String: data values that are made up or ordered sequences of characters.
+#String formatting: the process of inserting a custom string or variable in predefined text.
+#String indexing: allows us to access - or find and use - individual characters in a string based on the character’s position or index.
+#Substitution cipher: a method of encrypting where each plaintext character is replaced with a ciphertext character based on a specific key.
+#Telegram: an encryption app used for messaging and communication.
+#Usability: how easy or difficult it is to use something.
+#Efficiency: the technology makes the user’s life easier.
+#Error tolerance: the technology responds if a user makes an error.
+#Learnability: how easy the technology is to learn.
+#Memorability: it’s easy to remember how to use a technology.
+#Satisfaction: the user is happy with the experience.
+#Typecasting: converting a variable data type into a different data type.
+#Variable: containers that are used to store information (data) in a computer program. Variables are given a name so that they can be easily referenced and changed in a program.
+#Global variable: a variable that is declared outside of a function and therefore can be used anywhere in the program.
+#Local variable: a variable that is declared inside of a function and therefore can only be used within a function.
+#WhatsApp: an encryption app used for messaging and communication.
+#Wraparound characters: when the key value brings us to the end of the character list and we have to move back to the start or end of the list to reach our desired character.
